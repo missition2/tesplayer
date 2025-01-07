8278c22ca3a660b70a82bf11d1e9fbb9762f61ff
missition2 <157183354+missition2@users.noreply.github.com>
2025-01-07 19:06:36 -0300

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -69,10 +69,10 @@ function testSend() {
     const title = "Push title";
     const options = {
         body: "Additional text with some description",
-        icon: "https://example.com/images/icon.jpg",
-        image: "https://example.com/images/image.jpg",
+        icon: "https://missition2.github.io/tesplayer/images/push_icon.jpg",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/1920px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg",
         data: {
-            "url": "https://example.com/success",
+            "url": "https://missition2.github.io/tesplayer/?page=success",
             "message_id": "your_internal_unique_message_id_for_tracking"
         },
     };
