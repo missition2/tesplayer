2d4aff7d3713e69e05a07705887f549a27298eef
missition2 <157183354+missition2@users.noreply.github.com>
2025-01-07 18:22:45 -0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,32 +1,26 @@
-self.addEventListener('push', (event) => {
-    // PushData keys structure standart https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
-    let pushData = event.data.json();
-    if (!pushData || !pushData.title) {
-        console.error('Received WebPush with an empty title. Received body: ', pushData);
-    }
-    self.registration.showNotification(pushData.title, pushData)
-        .then(() => {
-            // You can save to your analytics fact that push was shown
-            // fetch('https://your_backend_server.com/track_show?message_id=' + pushData.data.message_id);
-        });
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+
+    const options = {
+        body: data.body,
+        icon: 'caminho/para/o/icon.png',
+        badge: 'caminho/para/o/badge.png',
+        data: {
+            url: data.url
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
 });
 
-self.addEventListener('notificationclick', function (event) {
+self.addEventListener('notificationclick', function(event) {
+    const url = event.notification.data.url;
     event.notification.close();
 
-    if (!event.notification.data) {
-        console.error('Click on WebPush with empty data, where url should be. Notification: ', event.notification)
-        return;
-    }
-    if (!event.notification.data.url) {
-        console.error('Click on WebPush without url. Notification: ', event.notification)
-        return;
-        
-    }
-
-    clients.openWindow(event.notification.data.url)
-        .then(() => {
-            // You can send fetch request to your analytics API fact that push was clicked
-            // fetch('https://your_backend_server.com/track_click?message_id=' + pushData.data.message_id);
-        });
+    // Abrir a URL associada à notificação
+    event.waitUntil(
+        clients.openWindow(url)
+    );
 });
