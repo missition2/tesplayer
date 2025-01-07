1611994e4b89035127625a974a7d6af540062dc2
missition2 <157183354+missition2@users.noreply.github.com>
2025-01-07 19:05:09 -0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,30 +1,29 @@
-self.addEventListener('push', function(event) {
-    console.log('Push recebido', event);
+self.addEventListener('push', (event) => {
+    let pushData = event.data.json();
 
-    // Extrair os dados da notificação
-    const pushData = event.data.json();
+    if (!pushData || !pushData.title) {
+        console.error('Received WebPush with an empty title. Received body: ', pushData);
+    }
 
-    // Exibir a notificação com os dados recebidos
-    event.waitUntil(
-        self.registration.showNotification(pushData.title, {
-            body: pushData.body,
-            icon: pushData.icon,
-            image: pushData.image,
-            data: {
-                url: pushData.url
-            }
-        })
-    );
+    self.registration.showNotification(pushData.title, pushData).then(() => {
+        // Você pode adicionar alguma análise ou acompanhamento de quando a notificação foi exibida.
+    });
 });
 
-self.addEventListener('notificationclick', function(event) {
-    console.log('Notificação clicada', event);
-
-    // Fechar a notificação
+self.addEventListener('notificationclick', function (event) {
     event.notification.close();
 
-    // Abrir a URL associada à notificação
-    event.waitUntil(
-        clients.openWindow(event.notification.data.url)
-    );
+    if (!event.notification.data) {
+        console.error('Click on WebPush with empty data. Notification: ', event.notification);
+        return;
+    }
+
+    if (!event.notification.data.url) {
+        console.error('Click on WebPush without url. Notification: ', event.notification);
+        return;
+    }
+
+    clients.openWindow(event.notification.data.url).then(() => {
+        // Aqui você pode enviar dados de clique para o seu servidor de análise.
+    });
 });
